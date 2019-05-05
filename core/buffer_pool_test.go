eb6f20e2108c1db5863b1406e605068d26553302
ocowchun <ocowchun@gmail.com>
2019-05-05 20:16:07 +0800

    extract dummy_replacer

--- core/buffer_pool_test.go
+++ core/buffer_pool_test.go
@@ -1,33 +1,10 @@
 package core
 
 import (
-	"errors"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
-
-type DummyReplacer struct {
-	frameIndices []uint32
-	pinnedIdxMap map[uint32]bool
-}
-
-func (d *DummyReplacer) Insert(frameIdx uint32) {
-	d.frameIndices = append(d.frameIndices, frameIdx)
-}
-
-func (d *DummyReplacer) Victim() (uint32, error) {
-	if len(d.frameIndices) > 0 {
-		frameIdx := d.frameIndices[0]
-		d.frameIndices = d.frameIndices[1:]
-		return frameIdx, nil
-	}
-	return 0, errors.New("no victim to evict")
-}
-
-func (d *DummyReplacer) Erase(frameIdx uint32) {
-	d.pinnedIdxMap[frameIdx] = true
-}
 
 type DummyPager struct {
 	body []byte
